10da3818ecc7435192a174ab1362663c07976e10
Oren Arones Paz <20a3110207@untels.edu.pe>
2023-01-02 11:01:53 -0500

    EF Parte02 - Producto - Arones

--- final_app/models.py
+++ final_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class Producto(models.Model):
+    codigo = models.CharField(max_length = 150)
+    nombre = models.CharField(max_length = 150)
+    precio_compra = models.FloatField(max_length = 150)
+    precio_venta = models.FloatField(max_length = 150)
+    fecha_compra = models.DateField()
+    fecha_registro = models.DateField(default=datetime.now())
+    estado = models.CharField(max_length = 15)
